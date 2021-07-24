ea7bea2310c1f13826d1dd8bf63134c788c89dfc
FranciscaFigueiredo <fraan928@gmail.com>
2021-07-24 04:21:19 -0300

    Sum of values ​​shown in HTML

--- scripts.js
+++ scripts.js
@@ -1,12 +1,27 @@
+let valorPedido = 0;
+let precoPrato = 0;
+let precoBebida = 0;
+let precoSobremesa = 0;
+
+
 function selecionarPrato (prato) {
     const selecionado = document.querySelector(".prato-selecionado");
 
     if (selecionado !== null) {
         selecionado.classList.remove("prato-selecionado");
-        selecionado.classList.remove("icone");
+        precoPrato = Number(selecionado.querySelector(".valor-prato").innerHTML.replace(',', '.'))
     }
     prato.classList.toggle("prato-selecionado");
-    prato.classList.toggle("icone");
+
+    if(precoPrato === 0) {
+        valorPedido += Number(prato.querySelector(".valor-prato").innerHTML.replace(',', '.'));
+        precoPrato = 1;
+    }
+    else {
+        valorPedido = valorPedido - precoPrato;
+        valorPedido += Number(prato.querySelector(".valor-prato").innerHTML.replace(',', '.'));
+    }
+    console.log(valorPedido);
 }
 
 function selecionarBebida (bebida) {
@@ -14,8 +29,19 @@ function selecionarBebida (bebida) {
 
     if (selecionada !== null) {
         selecionada.classList.remove("bebida-selecionada");
+        precoBebida = Number(selecionada.querySelector(".valor-bebida").innerHTML.replace(',', '.'))
     }
     bebida.classList.toggle("bebida-selecionada");
+
+    if(precoBebida === 0) {
+        valorPedido += Number(bebida.querySelector(".valor-bebida").innerHTML.replace(',', '.'));
+        precoBebida = 1;
+    }
+    else {
+        valorPedido = valorPedido - precoBebida;
+        valorPedido += Number(bebida.querySelector(".valor-bebida").innerHTML.replace(',', '.'));
+    }
+    console.log(valorPedido);
 }
 
 function selecionarSobremesa (sobremesa) {
@@ -23,7 +49,23 @@ function selecionarSobremesa (sobremesa) {
 
     if (selecionada !== null) {
         selecionada.classList.remove("sobremesa-selecionada");
+        precoSobremesa = Number(selecionada.querySelector(".valor-sobremesa").innerHTML.replace(',', '.'))
     }
     sobremesa.classList.toggle("sobremesa-selecionada");
+
+    if(precoSobremesa === 0) {
+        valorPedido += Number(sobremesa.querySelector(".valor-sobremesa").innerHTML.replace(',', '.'));
+        precoSobremesa = 1;
+    }
+    else {
+        valorPedido = valorPedido - precoSobremesa;
+        valorPedido += Number(sobremesa.querySelector(".valor-sobremesa").innerHTML.replace(',', '.'));
+    }
+    console.log(valorPedido);
 }
 
+function fecharPedido() {
+    if (precoPrato !== 0 && precoBebida !== 0 && precoSobremesa !== 0) {
+
+    }
+}
