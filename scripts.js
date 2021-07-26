224fa32d6360fcf42cbe0250d8f7b35917e9f488
FranciscaFigueiredo <fraan928@gmail.com>
2021-07-26 02:16:03 -0300

    Shipping order with item name and values

--- scripts.js
+++ scripts.js
@@ -1,27 +1,36 @@
 let valorPedido = 0;
-let precoPrato = 0;
-let precoBebida = 0;
-let precoSobremesa = 0;
-
+let precoPrato = null;
+let precoBebida = null;
+let precoSobremesa = null;
+let pedido1 = "";
+let pedido2 = "";
+let pedido3 = "";
+const botaoFecharPedido = document.querySelector(".fechar-pedido");
 
 function selecionarPrato (prato) {
     const selecionado = document.querySelector(".prato-selecionado");
 
     if (selecionado !== null) {
         selecionado.classList.remove("prato-selecionado");
+        //para armazenar o valor e retirar do cálculo se for selecionado outro prato
         precoPrato = Number(selecionado.querySelector(".valor-prato").innerHTML.replace(',', '.'))
     }
     prato.classList.toggle("prato-selecionado");
 
-    if(precoPrato === 0) {
+    if(precoPrato === null) {
         valorPedido += Number(prato.querySelector(".valor-prato").innerHTML.replace(',', '.'));
-        precoPrato = 1;
+        //para mudar o valor 0 anterior, para identificar na função fecharPedido que o campo já tem 1 item selecionado 
+        precoPrato = 0;
     }
     else {
         valorPedido = valorPedido - precoPrato;
         valorPedido += Number(prato.querySelector(".valor-prato").innerHTML.replace(',', '.'));
     }
+    // pegando o nome do prato selecionado
+    pedido1 = document.querySelector(".prato-selecionado strong").innerHTML;
     console.log(valorPedido);
+
+    verificarSelecao();     // verifica se tem 1 elemento marcado em cada tipo de alimento
 }
 
 function selecionarBebida (bebida) {
@@ -29,19 +38,25 @@ function selecionarBebida (bebida) {
 
     if (selecionada !== null) {
         selecionada.classList.remove("bebida-selecionada");
+        //para armazenar o valor e retirar do cálculo se for selecionada outra bebida
         precoBebida = Number(selecionada.querySelector(".valor-bebida").innerHTML.replace(',', '.'))
     }
     bebida.classList.toggle("bebida-selecionada");
 
-    if(precoBebida === 0) {
+    if(precoBebida === null) {
         valorPedido += Number(bebida.querySelector(".valor-bebida").innerHTML.replace(',', '.'));
-        precoBebida = 1;
+        //para mudar o valor 0 anterior, para identificar na função fecharPedido que o campo já tem 1 item selecionado 
+        precoBebida = 0;
     }
     else {
         valorPedido = valorPedido - precoBebida;
         valorPedido += Number(bebida.querySelector(".valor-bebida").innerHTML.replace(',', '.'));
     }
+    // pegando o nome da bebida selecionada
+    pedido2 = document.querySelector(".bebida-selecionada strong").innerHTML;
     console.log(valorPedido);
+
+    verificarSelecao();     // verifica se tem 1 elemento marcado em cada tipo de alimento
 }
 
 function selecionarSobremesa (sobremesa) {
@@ -49,23 +64,61 @@ function selecionarSobremesa (sobremesa) {
 
     if (selecionada !== null) {
         selecionada.classList.remove("sobremesa-selecionada");
+        //para armazenar o valor e retirar do cálculo se for selecionada outra sobremesa
         precoSobremesa = Number(selecionada.querySelector(".valor-sobremesa").innerHTML.replace(',', '.'))
     }
     sobremesa.classList.toggle("sobremesa-selecionada");
 
-    if(precoSobremesa === 0) {
+    if(precoSobremesa === null) {
         valorPedido += Number(sobremesa.querySelector(".valor-sobremesa").innerHTML.replace(',', '.'));
-        precoSobremesa = 1;
+        //para mudar o valor 0 anterior, para identificar na função fecharPedido que o campo já tem 1 item selecionado
+        precoSobremesa = 0;
     }
     else {
+        //retirando o valor do campo selecionado anteriormente
         valorPedido = valorPedido - precoSobremesa;
         valorPedido += Number(sobremesa.querySelector(".valor-sobremesa").innerHTML.replace(',', '.'));
     }
+    // pegando o nome da sobremesa
+    pedido3 = document.querySelector(".sobremesa-selecionada strong").innerHTML;
     console.log(valorPedido);
+
+    verificarSelecao();     // verifica se tem 1 elemento marcado em cada tipo de alimento
 }
 
-function fecharPedido() {
-    if (precoPrato !== 0 && precoBebida !== 0 && precoSobremesa !== 0) {
-
+function verificarSelecao() {
+    if (precoPrato !== null && precoBebida !== null && precoSobremesa !== null) {
+        ativarBotao();
     }
+}
+
+function ativarBotao() {
+    botaoFecharPedido.classList.add("botao-ativo");
+    document.querySelector(".botao-ativo").innerHTML="Fechar Pedido";
+    //confirmarPedido();
+    console.log(botaoFecharPedido)
+}
+
+function confirmarPedido() {
+    //se todos os 3 campos têm 1 item selecionado
+    // valorPedido = valorPedido.toFixed(2);
+
+    const confirmar = document.querySelector(".confirmar-pedido");
+
+    confirmar.classList.toggle("hidden");
+    document.querySelector(".pedido").innerHTML=`<br><strong>- Prato:</strong>${pedido1} <br><strong>- Bebida: </strong>${pedido2} <br><strong>- Sobremesa: </strong>${pedido3} <br><br><strong>Valor: R$ </strong>${valorPedido}<br><br>`;
+}
+
+function fecharCarrinho() {
+    const cancelar = document.querySelector(".confirmar-pedido");
+    // esconde a area do carrinho
+    cancelar.classList.toggle("hidden");
+    ativarBotao();
+}
+
+function enviarPedido() {
+    linkEnvioWhats = encodeURI(`*Pedido:*\n*- Prato:* ${pedido1} \n*- Bebida:* ${pedido2} \n*- Sobremesa:* ${pedido3} \n*Valor:* R$ ${valorPedido}`);
+
+    //link para envio direto no WhatsApp já com a mensagem de descrição do pedido
+    window.location.href = "https://wa.me/1111111111111?text=" + linkEnvioWhats;
 }
